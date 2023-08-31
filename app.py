75e6e9c5f69eee83d4fc3a0b0099dbd661cd707a
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2023-08-31 21:13:46 +0530

    files added

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, jsonify
+import openai
+import os
+
+app = Flask(__name__)
+
+# Replace 'YOUR_OPENAI_API_KEY' with your actual OpenAI API key
+openai.api_key = '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'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api', methods=['POST'])
+def api():
+     message = request.form.get("message")
+
+       
+     completion = openai.ChatCompletion.create(
+     model="gpt-3.5-turbo",
+     messages=[
+        {"role": "user", "content": message}
+     ]
+     )
+     print("Chat response:", completion.choices[0].message.content)
+     return jsonify({"message": completion.choices[0].message['content']})
+     
+
+if __name__ == '__main__':
+    app.run()
+
+
+ 
